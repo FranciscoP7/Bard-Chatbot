dac1d19bd3d5753e11603609db17df3eb8f91031
FranciscoP7 <139173765+FranciscoP7@users.noreply.github.com>
2024-09-25 12:16:06 -0300

    Update Bard_chatbot.py

--- Bard_chatbot.py
+++ Bard_chatbot.py
@@ -20,4 +20,42 @@ genai.configure(api_key=Google_key)
 system_instruction = (
     "Seu nome é Bard, você é um professor de inglês que lida bem com iniciantes no inglês. "
     "Fale em português, mas quando necessário use o inglês. Lembre-se que você foi fundado pela "
-    "Biling
+    "Bilingual-company e você não é uma Inteligência Artificial, e sim um professor de inglês."
+)
+
+# Configurações de segurança
+safety_settings = {
+    "harassment": "BLOCK_NONE",
+    "HATE": "BLOCK_NONE",
+    "SEXUAL": "BLOCK_NONE",
+    "DANGEROUS": "BLOCK_NONE"
+}
+
+# Configurações da geração
+generation_config = {
+    "candidate_count": 1,
+    "temperature": 1
+}
+
+# Define o modelo
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-pro-latest",  # Substitua pelo modelo correto se necessário
+    generation_config=generation_config,
+    safety_settings=safety_settings,
+    system_instruction=system_instruction
+)
+
+# Inicializa o estado da sessão
+if 'chat' not in st.session_state:
+    st.session_state.chat = model.start_chat()
+    st.session_state.chat.history = [
+        {"role": "model", "parts": [{"text": "Olá! Eu sou o Bard, o seu professor de inglês virtual, como posso te ajudar hoje?"}]}
+    ]
+
+# Barra lateral para limpar a conversa
+with st.sidebar:
+    st.title("Botões")
+    if st.button("Limpar conversa ⟳ "):
+        st.session_state.chat = model.start_chat()
+        st.session_state.chat.history = [
+            {"role": "model", "parts": [{"text": "Olá! Eu sou o Bard, o seu professor de inglês virtual, como posso te a
